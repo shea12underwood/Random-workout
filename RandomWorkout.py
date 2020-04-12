01eda9163a5e1cafa77ce15c52a309a535f7af4e
unknown <shea12underwood>
2020-04-12 17:44:35 -0400

    Rewrote the program to only give one exercise. Program now also saves that exercise to a csv file two directories up called sample.csv

--- RandomWorkout.py
+++ RandomWorkout.py
@@ -1,4 +1,6 @@
 import random
+import csv
+from datetime import datetime
 
 #List of all exercises
 MuscleGroups = ['Calves', 'Hamstrings', 'Quads', 'Glutes', 'Abs', 'Biceps', 'Triceps', 'Chest', 'Shoulders', 'Lats']
@@ -9,119 +11,31 @@ ExerciseDict = {'Calves':['Standing Calf Raises', 'Incline Walk/Run'],
                 'Abs' : ['Mountain Climbers','Planks','Lying Leg Lifts', 'Bicycle Crunches', 'Regular Crunches','Russian Twist','Oblique Crunches','Plate Lowers - Legs off the ground','Reverse Crunches', 'Hanging Knee Lifts','Hanging Knee Twists','Jack Knives'],
                 'Biceps' : ['Supinated Rows','EZ-Bar Curls Wide', 'EZ-Bar Curls Close', 'Hammer Curls', 'Close Grip Chin-ups', 'Hammer-Supinated Curls'],
                 'Triceps' : ['Tricep Kickback', 'Single Arm Dips', 'Powerbombs', 'Skull-Crushers'],
-                'Chest' : ['Pushups', 'Pushups in a circle', '90 degree holds', 'Decline Pushups','Slow Negative Explosive Pusups', 'Dumbbell Chest Press'],
+                'Chest' : ['Pushups', 'Pushups in a circle', '90 degree holds', 'Decline Pushups','Slow Negative Explosive Pushups', 'Dumbbell Chest Press'],
                 'Shoulders' : ['Pike Pushups', 'Overhead Press', 'Arnold Press', 'Front Raise','Horizontal Raise'],
-                'Lats' : ['Dumbbell Lat Pulls', 'Barbell Rows', 'Pull-ups'],
+                'Back/Lats' : ['Dumbbell Lat Pulls', 'Barbell Rows', 'Pull-ups', 'Renegade Row Reaches', 'Dumbbell Pull-Overs'],
                 'Extra' : ['Scalp Pushups', 'Shrugs', 'Upright Row'],
 }
 
+finishedWorkout = False
+
+while finishedWorkout == False:  
+    exerciseList = ['Standing Calf Raises','Squat Jumps','Single Leg Hip Raise','Wall Sit','Pistol Squat', 'Lunges', 'Bulgarian Split Squat','Squats','Step Ups', 'Side Lunges','Mountain Climbers','Planks','Lying Leg Lifts', 'Bicycle Crunches', 'Regular Crunches','Russian Twist','Oblique Crunches','Reverse Crunches','Jack Knives','Pushups', 'Pushups in a circle', '90 degree holds', 'Decline Pushups','Slow Negative Explosive Pushups','Scalp Pushups','Pike Pushups']
+    randomExercise = random.choice(exerciseList)
+    randomRange = random.randint(1,21)
+
+    print(f'Your random exercise is {randomExercise} for {randomRange}')
+    print('')
 
 
-# LegList = ['Squats','Front Squats','Lunges','Calf Raises','Bulgarian Split Squats','Romanian Dead Lifts','Squat Jumps','Glute Bridge','Single Leg Hip-Raises','Dumbbell Step-Ups', 'Wall-Sits']
-# UpperList = ['Dumbbell Lat Pulls','Reverse/Neutral Grip Barbell Rows','Pushups','Dumbbell Chest Press','Overhead Press','Shrugs','Chin-ups/Pullups']
-# IsoList = ['Arnold Press','Horizontal Lat Raise','Front Raises', 'Supinated Curls','Hammer Curls', 'Single Arm Dips','Powerbombs','Tricep Kickbacks','Scalp Pushups']
-# AbList = ['Mountain Climbers','Planks','Lying Leg Raises', 'Bicycle Crunches','Regular Crunches','Russian Twist','Oblique Crunches','Plate Lowers - Legs off the ground','Reverse Crunches', 'Hanging Knee Lifts']
+    a_file = open("../../sample.csv", "a",newline='')
+    writer = csv.writer(a_file)
+    writer.writerow([randomExercise, randomRange, datetime.now()])
 
+    a_file.close()
 
-# TotalList = LegList + UpperList + IsoList + AbList
-
-# LegNum = 1
-# UpperNum = 1
-# IsoNum = 1
-# AbNum = 2
-
-# #Checks to see if a user wants to change the default workout
-# LegsChosen = False
-# UpperChosen = False
-# IsoChosen = False
-# AbChosen = False
-# ChangeChosen = False
-# print(f"The current workout will have {LegNum} Leg exercise, {UpperNum} Upper body exercise, {IsoNum} Isometric exercise, and {AbNum} Ab exercises.")
-# print("")
-# ChangeWorkout = input("Would you like to change this? Please type 'Y' or 'N':  ")
-# while ChangeChosen == False:
-#     if ChangeWorkout == 'Y' or ChangeWorkout == 'N':
-#         ChangeChosen = True
-#         pass
-#     else:
-#         ChangeWorkout = input("Please Type a capital Y, or a capital N.")
-
-# #Path if the user wants to change the workout
-# if ChangeWorkout =='Y':
-#     while LegsChosen == False:
-#         try:
-#             LegNum = int(input("How many Leg exercises do you want to do? Please type a number 0 through 10:  "))
-#             if -1 < 0 + LegNum <11:
-#                 LegsChosen = True
-#             else:
-#                 print("That was not a number 0 through 10!")
-#                 LegsChosen = False
-#         except:
-#             print("That is not a valid option!")
-#     while UpperChosen == False:
-#         try:
-#             UpperNum = int(input("How many Upper exercises do you want to do? Please type a number 0 through 10:  "))
-#             if -1 < 0 + UpperNum <11:
-#                 UpperChosen = True
-#             else:
-#                 print("That was not a number 0 through 10!")
-#                 UpperChosen = False
-#         except:
-#             print("That is not a valid option!")
-#     while IsoChosen == False:
-#         try:
-#             IsoNum = int(input("How many Isometric exercises do you want to do? Please type a number 0 through 10:  "))
-#             if -1 < 0 + IsoNum <11:
-#                 IsoChosen = True
-#             else:
-#                 print("That was not a number 0 through 10!")
-#                 IsoChosen = False
-#         except:
-#             print("That is not a valid option!")
-#     while AbChosen == False:
-#         try:
-#             AbNum = int(input("How many Ab exercises do you want to do? Please type a number 0 through 10:  "))
-#             if -1 < 0 + AbNum <11:
-#                 AbChosen = True
-#             else:
-#                 print("That was not a number 0 through 10!")
-#                 AbChosen = False
-#         except:
-#             print("That is not a valid option!")
-
-
-
-# #Path if the user wants to keep the default workout
-
-# print("TODAY'S RANDOMIZED WORKOUT")
-# print("===========================")
-# print("")
-# print("Your Leg exercises are:")
-# LegChoices = random.sample(LegList, LegNum)
-# for i in LegChoices:
-#     print(i)
-# print("")
-# print("")
-# print("Your Upper exercises are:")
-# UpperChoices = random.sample(UpperList, UpperNum)
-# for i in UpperChoices:
-#     print(i)
-# print("")
-# print("")
-# print("Your Isometric exercises are:")
-# IsoChoices = random.sample(IsoList, IsoNum)
-# for i in IsoChoices:
-#     print(i)
-# print("")
-# print("")
-# print("Your Ab exercises are:")
-# AbChoices = random.sample(AbList, AbNum)
-# for i in AbChoices:
-#     print(i)
-# print("")
-# print("")
-# print("Your extra exercise is:")
-# print(random.choice(TotalList))
-# print("")
-# print("")
-# input("Press enter to continue...")
+    userDecision = input("Press y for another exercise or Press any key to close.")
+    if userDecision == 'y':
+        finishedWorkout = False
+    else:
+        finishedWorkout = True
